f96e0c705ade9e35a3429d6af811c439cba0e748
mohiulislam <68007928+mohiulislam@users.noreply.github.com>
2022-06-10 18:12:27 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 let x = document.getElementById("hamb");
+
 function toggle(){
-    if (getComputedStyle(x).getPropertyValue("display") === "none"){
-       x.style.display = "unset";
+    if (getComputedStyle(x).getPropertyValue("right") === "-200px"){
+       x.classList.add("hamb_apear");
        document.getElementById("bars").className="fa-solid fa-xmark";
     }
     else
     {
-        x.style.display = "none";
+        x.classList.remove("hamb_apear")
         document.getElementById("bars").className="fa-solid fa-bars";
     }
 }
