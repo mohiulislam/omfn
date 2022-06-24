1fe8199e6e30809b77adbaafa568cb62f718135b
mohiulislam <mohiulislam900@gmail.com>
2022-06-25 01:27:06 +0600

    2nd commit

--- js/index.js
+++ js/index.js
@@ -1,42 +1,34 @@
-// var elm = document.querySelector("#hamb").children[0];
-//  i = 0;
-// function hamb() {
+var elm = document.querySelector("#hamb").children[0];
+ i = 0;
+function hamb() {
    
-//     j = 0;
-//     if (getComputedStyle(elm.children[0]).getPropertyValue("transform") === "matrix(1, 0, 0, 1, 165, 0)") {
-//         apear();
-//         var myInterval = setInterval(apear, 150);
-//     }
+    j = 0;
+    if (getComputedStyle(elm.children[0]).getPropertyValue("transform") === "matrix(1, 0, 0, 1, 165, 0)") {
+        apear();
+        var myInterval = setInterval(apear, 150);
+    }
 
-//     else {
-//         disapear();
-//         var myIntervala = setInterval(disapear, 150);
-//     }
+    else {
+        disapear();
+        var myIntervala = setInterval(disapear, 150);
+    }
 
-//     function apear() {
-//         elm.children[i].style = "transform: unset";
-//         i++;
-//         if (i >= 5) {
-//             clearInterval(myInterval);
-//         }
-//     }
+    function apear() {
+        elm.children[i].style = "transform: unset";
+        i++;
+        if (i >= 5) {
+            clearInterval(myInterval);
+        }
+    }
 
-//     function disapear() {
+    function disapear() {
 
-//         elm.children[j].style = "transform: translateX(100%)";
-//         j++;
-//         if (j >= 5) {
-//             clearInterval(myIntervala);
-//         }
-//     }
-// }
-$(document).ready(function(){
+        elm.children[j].style = "transform: translateX(100%)";
+        j++;
+        if (j >= 5) {
+            clearInterval(myIntervala);
+        }
+    }
+}
 
-$("#bars").click(function(){
-    $("#hamb").animate({
-      right: '180px',
-    });
-  });
-
-  });
  
